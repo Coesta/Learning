56544db71629cc5de8b64bd1c607430895c26c36
Leonardo <Coesta@users.noreply.github.com>
2018-06-08 21:59:46 -0300

    Etapa 5. To create instance of a single album

--- Android/cardview/1_cardview_and_recyclerview/app/src/main/java/com/coesta/cardviewandrecyclerviewsample/ui/MainActivity.java
+++ Android/cardview/1_cardview_and_recyclerview/app/src/main/java/com/coesta/cardviewandrecyclerviewsample/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.coesta.cardviewandrecyclerviewsample;
+package com.coesta.cardviewandrecyclerviewsample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.coesta.cardviewandrecyclerviewsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
