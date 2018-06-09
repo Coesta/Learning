f6ec7e03a2355889180adfeee072527fd6275947
Leonardo <coeeu@hotmail.com>
2018-06-09 11:26:30 -0300

    Pequenos ajustes

--- Android/cardview/1_cardview_and_recyclerview/app/src/main/java/com/coesta/cardviewandrecyclerviewsample/ui/MainActivity.java
+++ Android/cardview/1_cardview_and_recyclerview/app/src/main/java/com/coesta/cardviewandrecyclerviewsample/ui/MainActivity.java
@@ -1,15 +1,15 @@
 package com.coesta.cardviewandrecyclerviewsample.ui;
 
 import android.content.res.Resources;
+import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         initCollapsingToolbar();
 //        getSupportActionBar().hide();
