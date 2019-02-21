aa195b10859478d3cf194f704105754d5b524708
Leonardo Costa <Coesta@users.noreply.github.com>
2019-02-21 15:36:07 -0300

    Aulas Iniciais de Kotlin
    Aula inicial até Aula 15 (Seção 3)

---
+++ Kotlin/CursoKotlin/src/fundamentos/Comentarios.kt
@@ -0,0 +1,15 @@
+package fundamentos
+
+/**
+ * Meu comentário... com dois astericos... é interpretado
+ * @param args lista de parâmetros passados por linha
+ *
+ */
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+    print("tamanho:" + args.size.toString())
+
+    /*
+    Mais de uma linha.
+     */
+}
---
+++ Kotlin/CursoKotlin/src/fundamentos/Var.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    var a: Int
+    var b = 2  //o Kotlin define o tipo da variável por inferência
+
+    a = 10
+
+    print(a + b)
+}
