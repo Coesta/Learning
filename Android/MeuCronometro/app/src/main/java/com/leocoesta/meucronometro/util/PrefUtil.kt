843b16473475edbdef4a6077ef51259c8c65036b
Leonardo <coesta@users.noreply.github.com>
2019-03-20 11:25:50 -0300

    Parte final - Tutorial do aplicativo MeuCronometro Link para o último vídeo: https://www.youtube.com/watch?v=r--0DUwBS9c

--- Android/MeuCronometro/app/src/main/java/com/leocoesta/meucronometro/util/PrefUtil.kt
+++ Android/MeuCronometro/app/src/main/java/com/leocoesta/meucronometro/util/PrefUtil.kt
@@ -7,9 +7,11 @@ import com.leocoesta.meucronometro.MainActivity
 class PrefUtil {
 
     companion object {
+
+        private const val TIMER_LENGTH_ID = "com.leocoesta.meucronometro.timer_length"
         fun getTimerLength(context: Context): Int {
-            //placeholder
-            return 1
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getInt(TIMER_LENGTH_ID, 10)
         }
 
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.leocoesta.meucronometro.previous_timer_length_seconds_id"
