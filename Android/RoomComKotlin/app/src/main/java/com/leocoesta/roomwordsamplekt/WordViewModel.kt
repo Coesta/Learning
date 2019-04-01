566175c61600a8d8db29df8f96d5775ca5600b59
Leonardo Costa <Coesta@users.noreply.github.com>
2019-03-31 21:09:22 -0300

    Codelab - Android Room with a View - Kotlin 9 - Create the ViewModel

--- Android/RoomComKotlin/app/src/main/java/com/leocoesta/roomwordsamplekt/WordViewModel.kt
+++ Android/RoomComKotlin/app/src/main/java/com/leocoesta/roomwordsamplekt/WordViewModel.kt
@@ -1,2 +1,37 @@
 package com.leocoesta.roomwordsamplekt
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+
+        allWords = repository.allWords
+    }
+
+    private var parentJob = Job()
+
+    private val coroutineContext
+        get() = parentJob + Dispatchers.Main
+
+    private val scope = CoroutineScope(coroutineContext)
+
+    fun insert(word: Word) = scope.launch(Dispatchers.IO){
+        repository.insert(word)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
