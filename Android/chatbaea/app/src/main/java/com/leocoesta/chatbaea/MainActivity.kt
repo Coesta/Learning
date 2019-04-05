5f9eefa9b610764818e8d006fadb9e58c66f108d
Leonardo <coesta@users.noreply.github.com>
2019-04-05 12:28:00 -0300

    Parcial do ChatBaea

--- Android/chatbaea/app/src/main/java/com/leocoesta/chatbaea/MainActivity.kt
+++ Android/chatbaea/app/src/main/java/com/leocoesta/chatbaea/MainActivity.kt
@@ -9,16 +9,12 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_contatos -> {
+                message.setText(R.string.contatos)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_conta -> {
+                message.setText(R.string.conta)
                 return@OnNavigationItemSelectedListener true
             }
         }
---
+++ Android/chatbaea/app/src/main/java/com/leocoesta/chatbaea/SignInActivity.kt
@@ -0,0 +1,45 @@
+package com.leocoesta.chatbaea
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+
+    private val singInProviders =
+        listOf(AuthUI.IdpConfig.EmailBuilder()
+            .setAllowNewAccounts(true)
+            .setRequireName(true)
+            .build())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        account_sign_in.setOnClickListener {
+            val intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(singInProviders)
+                .setLogo(R.mipmap.ic_chatbaea)
+                .build()
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (requestCode == Activity.RESULT_OK) {
+
+            }
+        }
+    }
+}
