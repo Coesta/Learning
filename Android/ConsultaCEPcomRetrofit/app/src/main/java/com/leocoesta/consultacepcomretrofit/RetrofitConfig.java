6075d8c113de1df385f77c02661d0c0882c6c151
Leonardo <coesta@users.noreply.github.com>
2019-05-03 12:32:16 -0300

    Instalando o Jackson

--- Android/ConsultaCEPcomRetrofit/app/src/main/java/com/leocoesta/consultacepcomretrofit/RetrofitConfig.java
+++ Android/ConsultaCEPcomRetrofit/app/src/main/java/com/leocoesta/consultacepcomretrofit/RetrofitConfig.java
@@ -1,11 +1,17 @@
 package com.leocoesta.consultacepcomretrofit;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class RetrofitConfig {
 
+    private final Retrofit retrofit;
+
     public RetrofitConfig() {
-        new Retrofit.Builder().baseUrl("http://ws.matheuscastiglioni.com.br/ws/");
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://ws.matheuscastiglioni.com.br/ws/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
     }
 
 }
