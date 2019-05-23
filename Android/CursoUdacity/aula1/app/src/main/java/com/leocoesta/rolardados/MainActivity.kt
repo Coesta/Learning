84f7793633e3912034f989e91b8e9ab33194fc7e
Leonardo <coesta@users.noreply.github.com>
2019-05-23 10:17:57 -0300

    Curso Udacity Aula 21. Exercise: Adding the ImageView

--- Android/CursoUdacity/aula1/app/src/main/java/com/leocoesta/rolardados/MainActivity.kt
+++ Android/CursoUdacity/aula1/app/src/main/java/com/leocoesta/rolardados/MainActivity.kt
@@ -2,11 +2,37 @@ package com.leocoesta.rolardados
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rolarDadosBt : Button = findViewById(R.id.rolar_dados_bt)
+        rolarDadosBt.setOnClickListener {
+            rolarDados()
+        }
+    }
+
+    private fun rolarDados() {
+
+        val randomNumber = Random.nextInt(6) + 1
+        val resourceDrawable = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val imagemDado : ImageView = findViewById(R.id.dado_image)
+        imagemDado.setImageResource(resourceDrawable)
+
     }
 }
