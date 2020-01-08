59742fe7278a9bd899fe4ca969d20ee9bd0054b3
Leonardo Costa <coesta@users.noreply.github.com>
2020-01-08 15:54:00 -0300

    Parcial da Paginação com PagedList

--- Android/TesteNoticias/app/src/main/java/com/leocoesta/testenoticias/NoticiasDataSource.kt
+++ Android/TesteNoticias/app/src/main/java/com/leocoesta/testenoticias/NoticiasDataSource.kt
@@ -1,3 +1,40 @@
 package com.leocoesta.testenoticias
 
-//class NoticiasDataSource :
+import androidx.paging.ItemKeyedDataSource
+import com.leocoesta.testenoticias.api.NoticiasService
+import com.leocoesta.testenoticias.model.Item
+import com.leocoesta.testenoticias.model.NoticiasResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoticiasDataSource(
+    private val api: NoticiasService,
+    private val nextPage: String
+) : ItemKeyedDataSource<String, Item>() {
+
+
+
+    override fun loadInitial(
+        params: LoadInitialParams<String>,
+        callback: LoadInitialCallback<Item>
+    ) {
+        val scope = CoroutineScope(Dispatchers.Default)
+        scope.launch {
+            val feedInicial = api.obterFeedPrincipal().feed.falkor.items
+
+            callback.onResult(feedInicial)
+        }
+    }
+
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<Item>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getKey(item: Item): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<Item>) {}
+
+}
