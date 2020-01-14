605e480f4fd6290e91d2eaa680eda1448768f159
Leonardo Costa <coesta@users.noreply.github.com>
2020-01-14 15:43:58 -0300

    Codelab - DI com Dagger Etapa 9. Scoping Subcomponents

--- Android/android-dagger-master/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ Android/android-dagger-master/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,12 +3,15 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
 
     // Factory to create instances of the AppComponent
@@ -18,8 +21,9 @@ interface AppComponent {
         fun create(@BindsInstance context: Context): AppComponent
     }
 
-    // Classes that can be injected by this Component
-    fun inject(activity: RegistrationActivity)
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
 
+    // Classes that can be injected by this Component
     fun inject(activity: MainActivity)
 }
