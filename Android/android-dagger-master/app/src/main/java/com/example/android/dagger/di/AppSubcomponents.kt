b2631d2a8638401845ac181cf05aacd0fca3c7f6
Leonardo Costa <coesta@users.noreply.github.com>
2020-01-15 08:30:19 -0300

    Codelab - DI com Dagger Etapa 12 - Refactoring Login Flow

--- Android/android-dagger-master/app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
+++ Android/android-dagger-master/app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
@@ -1,10 +1,9 @@
 package com.example.android.dagger.di
 
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.Module
 
 // This module tells AppComponent which are its subcomponents
-@Module(subcomponents = [RegistrationComponent::class])
-class AppSubcomponents {
-
-}
+@Module(subcomponents = [RegistrationComponent::class, LoginComponent::class])
+class AppSubcomponents
