420ae02e2e016db9dbdd2e01262e541b9504f95f
Leonardo Costa <Coesta@users.noreply.github.com>
2020-02-17 09:07:22 -0300

    Aula 1 - Lição 2: primeiro app com viewcontroller

---
+++ iOS/curso-alura/swift-parte-1/aula-1/eggplant-app/eggplant-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-app
+//
+//  Created by Leonardo Costa on 17/02/20.
+//  Copyright © 2020 Leonardo Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
