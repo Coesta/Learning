252c344c92d16bf061121a3ecf811e2849575113
Leonardo Costa <Coesta@users.noreply.github.com>
2020-02-18 15:53:17 -0300

    Swift - parte 2: Aula 01 - Lição 05
    Uma tabela dinâmica e sobrescrevendo eventos de um componente de view

--- iOS/curso-alura/swift-parte-1/aula-1/tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
+++ iOS/curso-alura/swift-parte-1/aula-1/tabela-dinamica-refeicoes/tabela-dinamica-refeicoes/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let refeicoes = ["Churros", "Pizza", "Lasanha"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // número de linhas
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
 
+    // conteúdo
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao
+        
+        return celula
+    }
 }
 
