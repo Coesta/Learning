62570f3b5ab001b5fd45539a92fa2ed808295737
Leonardo Costa <Coesta@users.noreply.github.com>
2020-02-20 10:17:03 -0300

    Criando uma xib para cadastrar itens.

---
+++ iOS/curso-alura/swift-parte-1/aula-1/eggplant-app/eggplant-app/Controllers/AdicionarItensViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AdicionarItensViewController.swift
+//  eggplant-app
+//
+//  Created by Leonardo Costa on 20/02/20.
+//  Copyright © 2020 Leonardo Costa. All rights reserved.
+//
+
+import UIKit
+
+class AdicionarItensViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
