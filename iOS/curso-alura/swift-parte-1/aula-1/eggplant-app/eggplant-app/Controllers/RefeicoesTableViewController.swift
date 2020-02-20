b50896c3a032feb407ea975f0b48ff3d9cd96df4
Leonardo Costa <Coesta@users.noreply.github.com>
2020-02-20 14:22:51 -0300

    Adicionando um alerta ao longPress

--- iOS/curso-alura/swift-parte-1/aula-1/eggplant-app/eggplant-app/Controllers/RefeicoesTableViewController.swift
+++ iOS/curso-alura/swift-parte-1/aula-1/eggplant-app/eggplant-app/Controllers/RefeicoesTableViewController.swift
@@ -36,7 +36,14 @@ class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDeleg
             guard let indexPath = tableView.indexPath(for: celula) else { return }
             let refeicao = refeicoes[indexPath.row]
             
-            print("refeicao: \(refeicao.nome)")
+            let alerta = UIAlertController(title: refeicao.nome, message: "felicidade: \(refeicao.felicidade)",
+                preferredStyle: .alert)
+            
+            let botaoFechar = UIAlertAction(title: "Fechar", style: .cancel, handler: nil)
+            
+            alerta.addAction(botaoFechar)
+            
+            present(alerta, animated: true, completion: nil)
         }
     }
     
