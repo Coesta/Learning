430032523a62c08a0be98fffe95fe44975cc26ec
Leonardo Costa <coeeu@hotmail.com>
2020-05-09 17:37:06 -0300

    Código até a aula 22 - Tipos em JS: Boolean

--- web/Curso Web Moderno com JS com Leonardo Leitão/fundamentos/tipagemFraca.js
+++ web/Curso Web Moderno com JS com Leonardo Leitão/fundamentos/tipagemFraca.js
@@ -5,3 +5,8 @@ console.log(typeof qualquer)
 qualquer = 3.1854
 console.log(qualquer)
 console.log(typeof qualquer)
+
+// Evitar nomes genéricos ou siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto químico perigoso... kkk
