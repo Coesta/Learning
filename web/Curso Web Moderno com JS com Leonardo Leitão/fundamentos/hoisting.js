8742a9150e6ab1d4dba2b3f9f504e648d6a35f32
Leonardo Costa <coeeu@hotmail.com>
2020-05-10 22:31:40 -0300

    Código até a aula 37 - Função Vs Objeto

---
+++ web/Curso Web Moderno com JS com Leonardo Leitão/fundamentos/hoisting.js
@@ -0,0 +1,3 @@
+console.log('a = ', a)
+var a = 2 // hoisting (ou içamento numa tradução literal)
+console.log('a = ', a)
---
+++ web/Curso Web Moderno com JS com Leonardo Leitão/fundamentos/usandoLet1.js
@@ -0,0 +1,8 @@
+let numero1 = 1
+
+{
+    let numero = 2
+    console.log('dentro = ', numero)
+}
+
+console.log('fora = ', numero)
