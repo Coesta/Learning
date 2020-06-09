54306593a126722034d2611ddbe67b171010c141
Leonardo Costa <Coesta@users.noreply.github.com>
2020-06-09 11:31:35 -0300

    Código até a aula 348 - Implementando o Footer

---
+++ Web/Curso Web Moderno com JS com Leonardo Leitão/bootstrap/projeto-galeria/src/js/core/includes.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+
+const loadHtmlSuccessCallback = []
+
+export function onLoadHtmlSucess(callback) {
+    if(!loadHtmlSuccessCallback.includes(callback)) {
+        loadHtmlSuccessCallback.push(callback)
+    }
+}
+
+function loadIncludes(parent) {
+    if (!parent)
+        parent = 'body'
+
+    $(parent).find('[wm-include]').each(function (i, e) {   
+        const url = $(e).attr('wm-include')
+        $.ajax({
+            url,
+            success(data){
+                $(e).html(data)
+                $(e).removeAttr('wm-include')
+
+                loadHtmlSuccessCallback.forEach(callback => callback(data))
+                loadIncludes(e)
+            }
+        })
+    })
+}
+
+loadIncludes()
