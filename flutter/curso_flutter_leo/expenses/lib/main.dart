85f76a5cb426087bb186eba539b6334f2d1b2831
Leonardo Costa <Coesta@users.noreply.github.com>
2020-08-10 16:52:05 -0300

    [flutter] código até a aula 95 - usando ListView

---
+++ flutter/curso_flutter_leo/expenses/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:expenses/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Gráfico'),
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+    );
+  }
+}
