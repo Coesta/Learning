b21c83365b34eac09a9b8905c7593d4e18e76574
Leonardo Costa <Coesta@users.noreply.github.com>
2020-11-25 14:41:02 -0300

    codelab - flutter testing app

---
+++ Android/the-movie-db-kotlin/app/src/main/java/com/example/tmdbkotlin/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.tmdbkotlin.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ Android/the-movie-db-kotlin/app/src/main/java/com/example/tmdbkotlin/ui/moviedetail/MovieDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.tmdbkotlin.ui.moviedetail
+
+import androidx.lifecycle.ViewModel
+
+class MovieDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
